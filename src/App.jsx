89da89ab5b1eb7bcaf09adde7eb1c9457224d7c9
BeauX <fire-fly_7@hotmail.com>
2024-09-29 18:07:47 -0700

    ex 3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,35 @@ import NavBar from './components/NavBar';
 import MailboxForm from './components/MailboxForm';
 import MailboxList from './components/MailboxList';
 import MailboxDetails from './components/MailboxDetails';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
+  
+  
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route 
+          path="/" 
+          element={<main><h1>Post Office</h1></main>} 
+        />
+        <Route 
+          path="/mailboxes" 
+          element={<MailboxList mailboxes={mailboxes} />} 
+        />
+        <Route 
+          path="/new-mailbox" 
+          element={<MailboxForm onCreateMailbox={createMailbox} />} 
+        />
+        <Route 
+          path="/mailboxes/:mailboxId" 
+          element={<MailboxDetails mailboxes={mailboxes} />} 
+        />
+      </Routes>
+    </Router>
+  );
+}
+
 
 export default App;
